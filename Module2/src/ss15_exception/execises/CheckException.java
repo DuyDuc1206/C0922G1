cb2de655f3af03890249fe723e3349a8de268974
Your NameDuyDuc1206 <you@example.comduuyyduc@gmail.com>
2022-10-25 18:33:23 +0700

    setup

--- Module2/src/ss15_exception/execises/CheckException.java
+++ Module2/src/ss15_exception/execises/CheckException.java
@@ -2,13 +2,13 @@ package ss15_exception.execises;
 
 public class CheckException {
     public static void checkSide(int a) throws WrongInputException {
-        if (a<0){
-            throw new WrongInputException("Re-enter a number greater than 0");
+        if (a < 0) {
+            throw new WrongInputException("Number greater than 0");
         }
     }
-    public static void checkSumTwoSide(int[] arr){
-        for (int i = 0; i <arr.length ; i++) {
-            if
-        }
+
+    public static void checkSumTwoSide(int side1, int side2, int side3) throws WrongInputException {
+        if (side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1)
+            throw new WrongInputException("sum of 2 side is greater than 1 side ");
     }
 }
--- Module2/src/ss15_exception/execises/IllegalTriangleException.java
+++ Module2/src/ss15_exception/execises/IllegalTriangleException.java
@@ -1,30 +1,40 @@
 package ss15_exception.execises;
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class IllegalTriangleException {
     private static Scanner scanner = new Scanner(System.in);
-    private static List<Integer> sideList = new ArrayList<>();
+    static int i = 1;
     public static void main(String[] args) {
-        for (int i = 0; i < 3; i++) {
-            System.out.println("enter the side " + (i + 1));
-            IllegalTriangleException.enterSide();
-            sideList.add(i);
-        }
-        System.out.println(sideList);
-
-    }
-
-    public static void enterSide() {
         while (true) {
+            int side1 = enterSide();
+            int side2 = enterSide();
+            int side3 = enterSide();
             try {
-                int number = Integer.parseInt(scanner.nextLine());
-                CheckException.checkSide(number);
+                CheckException.checkSumTwoSide(side1, side2, side3);
+                System.out.println("These are three side of triangle");
                 break;
             } catch (WrongInputException e) {
+                i =1;
                 System.out.println(e.getMessage());
+                System.out.println("re-enter");
+            }
+        }
+    }
+
+    public static int enterSide() {
+        while (true) {
+            try {
+                System.out.println("Enter the side " + i + " of triangle ");
+                int number = Integer.parseInt(scanner.nextLine());
+                CheckException.checkSide(number);
+                i++;
+                return number;
+            } catch (WrongInputException e) {
+                System.out.println(e.getMessage());
+                System.out.println("Re-enter");
+            } catch (NumberFormatException e) {
+                System.out.println(e.getMessage());
+                System.out.println("Re-enter");
             }
         }
     }
