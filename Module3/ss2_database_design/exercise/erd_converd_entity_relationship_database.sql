47bc60d4b7d6bc7e8e7e52c38f344548f63d513d
Your NameDuyDuc1206 <you@example.comduuyyduc@gmail.com>
2022-11-15 19:25:44 +0700

    update

--- Module3/ss2_database_design/exercise/erd_converd_entity_relationship_database.sql
+++ Module3/ss2_database_design/exercise/erd_converd_entity_relationship_database.sql
@@ -1,58 +1,58 @@
 CREATE DATABASE if not exists hang_hoa;
 USE hang_hoa;
 create table nha_cung_cap(
-	id_nha_cc int auto_increment primary key,
-    ten_nha_cc varchar(50),
-    dia_chi varchar(50)
-    );
+  id_nha_cc int auto_increment primary key, 
+  ten_nha_cc varchar(50), 
+  dia_chi varchar(50)
+);
 create table sdt(
-	id int auto_increment primary key,
-    ten_sdt varchar(50),
-    id_nha_cung_cap int,
-    foreign key (id_nha_cung_cap) references nha_cung_cap(id_nha_cc)
-    ); 
+  id int auto_increment primary key, 
+  ten_sdt varchar(50), 
+  id_nha_cung_cap int, 
+  foreign key (id_nha_cung_cap) references nha_cung_cap(id_nha_cc)
+);
 create table phieu_xuat(
-	id_phieu_xuat int auto_increment primary key,
-    ngay_xuat date
-    );
+  id_phieu_xuat int auto_increment primary key, 
+  ngay_xuat date
+);
 create table phieu_nhap(
-	id_phieu_nhap int auto_increment primary key,
-    ngay_nhap date
-    );
+  id_phieu_nhap int auto_increment primary key, 
+  ngay_nhap date
+);
 create table vat_tu(
-	id_vat_tu int auto_increment primary key,
-    ten_vat_tu varchar(50)
-    );
+  id_vat_tu int auto_increment primary key, 
+  ten_vat_tu varchar(50)
+);
 create table don_dat_hang(
-	id_don_dat_hang int auto_increment primary key,
-    ngay_dat_hang date,
-    id_nha_cc int,
-    foreign key(id_nha_cc) references nha_cung_cap(id_nha_cc)	
-    );
+  id_don_dat_hang int auto_increment primary key, 
+  ngay_dat_hang date, 
+  id_nha_cc int, 
+  foreign key(id_nha_cc) references nha_cung_cap(id_nha_cc)
+);
 create table chi_tiet_phieu_xuat(
-	id_phieu_xuat int,
-    id_vat_tu int,
-    sl_xuat int,
-    dg_xuat double,
-    primary key(id_phieu_xuat,id_vat_tu),
-    foreign key(id_phieu_xuat) references phieu_xuat(id_phieu_xuat),
-    foreign key(id_vat_tu) references vat_tu(id_vat_tu)
-    );
+  id_phieu_xuat int, 
+  id_vat_tu int, 
+  sl_xuat int, 
+  dg_xuat double, 
+  primary key(id_phieu_xuat, id_vat_tu), 
+  foreign key(id_phieu_xuat) references phieu_xuat(id_phieu_xuat), 
+  foreign key(id_vat_tu) references vat_tu(id_vat_tu)
+);
 create table chi_tiet_phieu_nhap(
-	id_phieu_nhap int,
-    id_vat_tu int,
-    sl_nhap int,
-    dg_nhap double,
-    primary key(id_phieu_nhap,id_vat_tu),
-    foreign key (id_phieu_nhap) references phieu_nhap(id_phieu_nhap),
-    foreign key (id_vat_tu) references vat_tu(id_vat_tu)
-    );
+  id_phieu_nhap int, 
+  id_vat_tu int, 
+  sl_nhap int, 
+  dg_nhap double, 
+  primary key(id_phieu_nhap, id_vat_tu), 
+  foreign key (id_phieu_nhap) references phieu_nhap(id_phieu_nhap), 
+  foreign key (id_vat_tu) references vat_tu(id_vat_tu)
+);
 create table chi_tiet_don_dat_hang(
-	id_don_dat_hang int,
-    id_vat_tu int,
-    sl_vat_tu int,
-    primary key(id_don_dat_hang,id_vat_tu),
-    foreign key(id_don_dat_hang) references don_dat_hang(id_don_dat_hang),
-    foreign key(id_vat_tu) references vat_tu(id_vat_tu)
-    );
-    
+  id_don_dat_hang int, 
+  id_vat_tu int, 
+  sl_vat_tu int, 
+  primary key(id_don_dat_hang, id_vat_tu), 
+  foreign key(id_don_dat_hang) references don_dat_hang(id_don_dat_hang), 
+  foreign key(id_vat_tu) references vat_tu(id_vat_tu)
+);
+
--- Module3/ss2_database_design/practice/student_management_database.sql
+++ Module3/ss2_database_design/practice/student_management_database.sql
@@ -1,34 +1,38 @@
 create database student_management_database;
 use student_management_database;
 create table class(
-	classId int not null auto_increment primary key,
-    className varchar(60) not null,
-    startDate datetime not null,
-    status bit
+  classId int not null auto_increment primary key, 
+  className varchar(60) not null, 
+  startDate datetime not null, 
+  status bit
 );
 create table student(
-	studentId int not null auto_increment primary key,
-    studentName varchar(30) not null,
-    address varchar(50),
-    phone varchar(20),
-    status bit,
-    classId int,
-    foreign key(classId) references class ( classId)
+  studentId int not null auto_increment primary key, 
+  studentName varchar(30) not null, 
+  address varchar(50), 
+  phone varchar(20), 
+  status bit, 
+  classId int, 
+  foreign key(classId) references class (classId)
 );
 create table `subject`(
-	subId int not null auto_increment primary key,
-    subName varchar(30) not null,
-    credit tinyint not null default 1 check( credit>=1),
-    status bit default 1
+  subId int not null auto_increment primary key, 
+  subName varchar(30) not null, 
+  credit tinyint not null default 1 check(credit >= 1), 
+  status bit default 1
 );
 create table mark(
-	markId int not null auto_increment primary key,
-    mark float default 0 check ( mark between 0 and 100 ),
-    examTimes tinyint default 1,
-    subId int not null,
-    studentId int not null,
-    unique(subId,studentId),
-    foreign key(subId) references `subject`(subId),
-    foreign key(studentId) references student(studentId)
+  markId int not null auto_increment primary key, 
+  mark float default 0 check (
+    mark between 0 
+    and 100
+  ), 
+  examTimes tinyint default 1, 
+  subId int not null, 
+  studentId int not null, 
+  unique(subId, studentId), 
+  foreign key(subId) references `subject`(subId), 
+  foreign key(studentId) references student(studentId)
 );
-DROP database student_management_database;
+DROP 
+  database student_management_database;
