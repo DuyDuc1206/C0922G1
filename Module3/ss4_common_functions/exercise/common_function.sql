82ae799c4009f3ac577d4b5a0be736ea2994c39e
Your NameDuyDuc1206 <you@example.comduuyyduc@gmail.com>
2022-11-17 19:19:21 +0700

    update

--- Module3/ss4_common_functions/exercise/common_function.sql
+++ Module3/ss4_common_functions/exercise/common_function.sql
@@ -7,8 +7,7 @@ where Credit = (select max(Credit) from subject);
 
 -- Hiển thị các thông tin môn học có điểm thi cao nhất 
 select s.*, m.Mark as 'max point' from subject s
-join mark m
-on m.SubId = s.SubId
+join mark m on m.SubId = s.SubId
 where m.Mark =(select max(Mark) from mark);
 
 -- Hiển thị các thông tin sinh viên và điểm trung bình của mỗi sinh viên, xếp hạng theo thứ tự điểm giảm đần
@@ -16,5 +15,3 @@ select s.*, avg(m.Mark) as 'average' from student s, mark m
 where s.StudentId = m.StudentId
 group by StudentId
 order by average desc, s.StudentName asc;
-
-
